9ca4b3095bb422f830b63daaff04457d2771d759
Yash1361 <yashaggarwal3011@gmail.com>
2024-10-12 19:33:56 -0400

    Initial Commit

---
+++ TableMate/TableMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableMateApp.swift
+//  TableMate
+//
+//  Created by Yash Aggarwal on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TableMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TableMateTests/TableMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  TableMateTests.swift
+//  TableMateTests
+//
+//  Created by Yash Aggarwal on 12/10/24.
+//
+
+import Testing
+@testable import TableMate
+
+struct TableMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
